1ee40dd2511374e125da9894c50a8d6b20de438b
David Ciria <dciria@ackcent.com>
2024-03-13 16:07:41 +0100

    Changed server to flask

--- integration/alert_handler.py
+++ integration/alert_handler.py
@@ -3,181 +3,70 @@
 # --------------------------
 # HTTP server that read webhooks alerts send by the SIEM
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
-from alert_interpreter import JSONAlertInterpreter
+from flask import Flask, request, jsonify, render_template
 from datetime import datetime
+from alert_interpreter import JSONAlertInterpreter
 import logging
 
-server_port = 8080
+app = Flask(__name__)
 
-logging.basicConfig(
-    filename="requests.log", level=logging.INFO, format="%(asctime)s - %(message)s"
-)
+# logging.basicConfig(
+#     filename="requests.log", level=logging.INFO, format="%(asctime)s - %(message)s"
+# )
 json_alert_interpreter = JSONAlertInterpreter()
 
 
-class JSONRequestHandler(BaseHTTPRequestHandler):
+@app.route("/", methods=["GET"])
+def handle_hello_get():
+    html_params = {"request_host": request.host}
 
-    
-    def do_GET(self):
-        if self.path == "/":
-            self.handle_hello_get()
-        else:
-            self.send_response(404)
-            self.end_headers()
-            self.wfile.write(b"Not Found")
+    return render_template("doc.html", **html_params)
 
-    def do_POST(self):
-        if self.path == "/interpret":
-            self.handle_interpret()
-        elif self.path == "/add_schema":
-            self.handle_add_schema()
-        else:
-            self.send_response(404)
-            self.end_headers()
-            self.wfile.write(b"Not Found")
 
-    def handle_add_schema(self):
-        content_length = int(self.headers["Content-Length"])
-        post_data = self.rfile.read(content_length)
+@app.route("/add_schema", methods=["POST"])
+def handle_add_schema():
+    try:
+        json_data = request.get_json()
+        schema = json_data.get("schema")
+        use_case_id = json_data.get("use_case_id")
 
-        try:
-            json_data = json.loads(post_data.decode("utf-8"))
-
-            schema = json_data.get("schema")
-            use_case_id = json_data.get("use_case_id")
-
-            if schema and use_case_id:
-                if json_alert_interpreter.add_schema(schema, use_case_id):
-                    self.send_response(200)
-                    self.send_header("Content-type", "application/json")
-                    self.end_headers()
-                    self.wfile.write(
-                        json.dumps(
-                            {"status": "200", "message": "Schema added succesfully"}
-                        ).encode("utf-8")
-                    )
-                else:
-                    self.send_response(400)
-                    self.end_headers()
-                    self.wfile.write(
-                        b"Invalid JSON schema (schema does not follow Draft7 syntax)"
-                    )
-            else:
-                self.send_response(400)
-                self.end_headers()
-                self.wfile.write(
-                    b"Invalid JSON data (does not contain schema or use_case_id)"
+        if schema and use_case_id:
+            if json_alert_interpreter.add_schema(schema, use_case_id):
+                return (
+                    jsonify({"status": "200", "message": "Schema added succesfully"}),
+                    200,
                 )
-        except json.JSONDecodeError:
-            self.send_response(400)
-            self.end_headers()
-            self.wfile.write(b"Invalid JSON data")
-
-    def handle_interpret(self):
-        content_length = int(self.headers["Content-Length"])
-        post_data = self.rfile.read(content_length)
-
-        try:
-            json_data = json.loads(post_data.decode("utf-8"))
-
-            print(f"Alert recieved: {json_data}")
-            use_cases = json_alert_interpreter.interpret(json_data)
-
-            # Log timestamp, alert_received, and types detected
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            log_event = {"alert_recieved": json_data, "use_cases": use_cases}
-            log_message = (
-                f"{timestamp} - {json.dumps(log_event, separators=(',', ':'))}"
-            )
-            logging.info(log_message)
-
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(log_event).encode("utf-8"))
-
-        except json.JSONDecodeError:
-            self.send_response(400)
-            self.end_headers()
-            self.wfile.write(b"Invalid JSON data")
-
-    def handle_hello_get(self):
-
-        script = """
-            <script>
-                let add_schema_req = {
-                    "use_case_id": "example_id",
-                    "schema": {
-                        "type": "object",
-                        "required": [
-                            "attack_type"
-                        ],
-                        "properties": {
-                            "attack_type": {
-                                "type": "string",
-                                "enum": [
-                                    "phishing"
-                                ]
-                            }
-                        },
-                        "additionalProperties": false
-                    }
-                }
-                document.getElementById("add_schema_req").textContent = JSON.stringify(add_schema_req, undefined, 2);
-            </script>
-        """
-
-        html_content = f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Alert Handler</title>
-        </head>
-        <body>
-            <h1>Welcome to alert handler!</h1>
-            <p>HTTP server that read webhooks alerts send by the SIEM</p>
-            <h2>Routes</h2>
-
-            <h3>Add schema</h3>
-            <div><strong>POST</strong> http://localhost:{server_port}/add_schema</div>
-            <h4>Example request</h4>
-            <div style="background: black; color: white; padding: 5px; display: inline-block;">
-            <pre style="margin:0" id="add_schema_req">
-            </pre>
-            </div>
-            <h4>Example response</h4>
-
-            <h3>Interpret Alert</h3>
-            <div><strong>POST</strong> http://localhost:{server_port}/interpret</div>
-            <h4>Example request</h4>
-            <h4>Example response</h4>
-
-            {script}
-        </body>
-        </html>
-        """
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(html_content.encode("utf-8"))
+            else:
+                return "Invalid JSON schema (schema does not follow Draft7 syntax)", 400
+        else:
+            return "Invalid JSON data (does not contain schema or use_case_id)", 400
+    except Exception as e:
+        return "Invalid JSON data", 400
 
 
-def run_server(port=server_port):
-    server_address = ("", port)
-    httpd = HTTPServer(server_address, JSONRequestHandler)
-    print(f"Starting server on port {port}...")
-    httpd.serve_forever()
+@app.route("/interpret", methods=["POST"])
+def handle_interpret():
+    try:
+        json_data = request.get_json()
+        print(f"Alert received: {json_data}")
+        use_cases = json_alert_interpreter.interpret(json_data)
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_event = {"alert_received": json_data, "use_cases": use_cases}
+        log_message = f"{timestamp} - {log_event}"
+        logging.info(log_message)
+
+        return jsonify(log_event), 200
+    except Exception as e:
+        return "Invalid JSON data", 400
 
 
 if __name__ == "__main__":
-
     schema = {
         "type": "object",
         "required": ["attack_type"],
         "properties": {"attack_type": {"type": "string", "enum": ["phishing"]}},
         "additionalProperties": False,
     }
-    json_alert_interpreter.add_schema(schema, "phising_attack")
-    run_server()
+    json_alert_interpreter.add_schema(schema, "phishing_attack")
+    app.run(port=8080)
